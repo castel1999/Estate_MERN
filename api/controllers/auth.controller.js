3ee0758cfc8854554c0b54686ffff8f6f2a2234e
ldduc123 <ducld0510@gmail.com>
2024-03-25 00:09:17 +0700

    FE-BE sign-in function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import { errorHandler } from '../Utils/error.js';
 
 export const signup = async (req, res, next) => {
     const { userName, email, password } = req.body;
@@ -9,6 +12,25 @@ export const signup = async (req, res, next) => {
 
         await newUser.save();
         res.status(200).json({ message: "User created successfully!" });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "User not found!"));
+        const validpassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validpassword) return next(errorHandler(401, "wrong credential!"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie("accesss_token", token, { httpOnly: true, })
+            .status(200)
+            .json(rest);
 
     } catch (error) {
         next(error);
