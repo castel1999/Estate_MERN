af88d0f86a579223e85f529927db26df4f50b882
ldduc123 <ducld0510@gmail.com>
2024-04-03 23:35:26 +0700

    Add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import "dotenv/config";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser'
 
 mongoose.connect(process.env.MONGO_DB)
@@ -20,6 +21,7 @@ app.use(cookieParser());
 //route
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
 
 
 
---
+++ api/models/listing.model.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose'
+
+const listingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    regularPrice: {
+        type: String,
+        required: true,
+    },
+    discountPrice: {
+        type: String,
+        required: true,
+    },
+    bathrooms: {
+        type: String,
+        required: true,
+    },
+    bedrooms: {
+        type: String,
+        required: true,
+    },
+    furnished: {
+        type: Boolean,
+        required: true,
+    },
+    parking: {
+        type: Boolean,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    offer: {
+        type: Boolean,
+        required: true,
+    },
+    imageUrls: {
+        type: Array,
+        required: true,
+    },
+    userRef: {
+        type: String,
+        required: true,
+    },
+}, { timeseries: true })
+
+const Listing = mongoose.model('Listing', listingSchema);
+export default Listing;
